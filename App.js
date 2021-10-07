456c5650e8e0df3e0c8b2010dc35ac2a7d9c2d3a
laury <lauri.ryopponen@gmail.com>
2021-10-07 11:10:00 +0300

    Couple days of work on the app initialization

--- App.js
+++ App.js
@@ -1,26 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { FileSystem } from 'react-native-unimodules';
+import { FetchAppNames, Init, addFish, Drop } from './components/db';
 
-import Counter from './apps/count/App';
-import Calc from './apps/calc/App';
+var INSTALLED_APPS = [];
+var DOWNLOADED_APPS = [];
+var APP_REQUIREMENTS = [];
 
-export default function App() {
+Init().then(()=>{console.log('Database creation succeeded!');}).catch((err)=>{console.log('Database IS NOT initialized! ' + err);});
+
+ClearApps();
+CheckApps();
+
+GetAllApps();
+
+async function GetAllApps(){
+  try{
+    const DB_RESULT = await FetchAppNames();
+    const RESULT_ARRAY = DB_RESULT.rows._array;
+    RESULT_ARRAY.forEach(ELEMENT => {
+      INSTALLED_APPS.push(ELEMENT.name);
+    });
+  }
+  catch(ERR){
+    console.log(ERR);
+  }
+  INSTALLED_APPS.forEach(ELEMENT => {
+    if (DOWNLOADED_APPS.findIndex(x => x.name == ELEMENT) == -1) {
+      DlApp(ELEMENT);
+    }
+  });
+  DOWNLOADED_APPS.forEach(ELEMENT => {
+    CreateReqs(ELEMENT.name, ELEMENT.path);
+  })
+  console.log(APP_REQUIREMENTS);
+}
+
+function CreateReqs(APP_NAME, PATH) {
+  const STRING = 'require("' + PATH + '");';
+  const FUNC = new Function(STRING);
+  const OBJ = {
+    name: APP_NAME,
+    func: FUNC
+  }
+  APP_REQUIREMENTS.push(OBJ);
+  console.log(APP_NAME + ' require added');
+}
+
+async function DlApp(APP_NAME){
+  const PATH = FileSystem.documentDirectory + 'apps/' + APP_NAME;
+  await FileSystem.makeDirectoryAsync(PATH, { intermediates: true });
+  const REMOTE_URI = 'https://storage.googleapis.com/awis-apps/' + APP_NAME;
+  const { uri: LOCAL_PATH } = await FileSystem.downloadAsync(REMOTE_URI, PATH);
+  const OBJ = {
+    name: APP_NAME,
+    path: LOCAL_PATH
+  }
+  console.log(APP_NAME + " downloaded!");
+  DOWNLOADED_APPS.push(OBJ);
+}
+
+async function ClearApps(){
+  const PATH = FileSystem.documentDirectory + 'apps';
+  await FileSystem.deleteAsync(PATH);
+  await FileSystem.makeDirectoryAsync(PATH);
+  return console.log("Apps deleted!");
+}
+
+async function CheckApps(){
+  const PATH = FileSystem.documentDirectory + 'apps';
+  let DIR = await FileSystem.readDirectoryAsync(PATH)
+  .then(DIR => {
+    if (DIR.length > 0) {
+      DIR.forEach(ELEMENT => {
+        DOWNLOADED_APPS.push(ELEMENT);
+      });
+    }
+  })
+  .catch((err)=>{
+    console.log('Database IS NOT initialized! '+err);
+  });
+}
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.container}>
-        <Counter></Counter>
-      </View>
-      <View style={styles.container}>
-        <Calc></Calc>
-      </View>
-      <View style={styles.container}>
-      </View>
+    <View style={STYLES.container}>
+      <TouchableOpacity><Text>aaaaaaaaa</Text></TouchableOpacity>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const STYLES = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -28,3 +98,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
---
+++ components/db.js
@@ -0,0 +1,68 @@
+import * as SQLite from 'expo-sqlite';
+
+const db=SQLite.openDatabase('awis.db');
+
+export const Init=()=>{
+    const promise=new Promise((resolve, reject)=>{
+        db.transaction((tx)=>{
+            tx.executeSql('create table if not exists apps(id integer not null primary key, name text not null);',      
+            [],
+            ()=>{resolve(); },
+            (_,err)=>{reject(err);}
+            );
+        });
+    });
+    return promise;
+};
+
+export const Drop=()=>{
+    const promise=new Promise((resolve, reject)=>{
+        db.transaction((tx)=>{
+            tx.executeSql('drop table apps;',      
+            [],
+            ()=>{resolve(); },
+            (_,err)=>{reject(err);}
+            );
+        });
+    });
+    return promise;
+};
+
+export const FetchAppNames = () => {
+    const promise = new Promise((resolve, reject)=>{
+        db.transaction((tx)=>{
+            tx.executeSql('select * from apps;',
+            [],
+            (tx, result) => { 
+                resolve(result);
+            },
+            (tx, err) => {
+                reject(err);
+            });
+        });
+    });
+    return promise;
+};
+
+export const addFish=(id)=>{
+    const promise=new Promise((resolve, reject)=>{
+        db.transaction((tx)=>{
+            //Here we use the Prepared statement, just putting placeholders to the values to be inserted
+            tx.executeSql('insert into apps(id, name) values(?,?);',
+            //And the values come here
+            [id, "calc"],
+            //If the transaction succeeds, this is called
+            (_, result)=>{
+                resolve(result);
+                console.log("Success");
+            },
+            //If the transaction fails, this is called
+            (_,err)=>{
+                reject(err);
+                console.log("NOPE");
+            }
+            );
+        });
+    });
+    return promise;
+};
